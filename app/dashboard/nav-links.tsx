14f24e6abd5d7f8b5a508b0d41ba339f8863f378
mubinakib5 <mubinakib027@gmail.com>
2024-12-23 12:08:07 +0600

    fixed nav-links

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -8,10 +8,28 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-// ...
+
+const links = [
+  {
+    name: 'Home',
+    href: '/',
+    icon: HomeIcon,
+  },
+  {
+    name: 'Documents',
+    href: '/documents',
+    icon: DocumentDuplicateIcon,
+  },
+  {
+    name: 'Users',
+    href: '/users',
+    icon: UserGroupIcon,
+  },
+];
 
 export default function NavLinks() {
   const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -24,13 +42,14 @@ export default function NavLinks() {
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+              }
             )}
+          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </Link >
+          </Link>
         );
-})}
+      })}
     </>
   );
 }
